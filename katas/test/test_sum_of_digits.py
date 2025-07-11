923cd733469cfae43b01e52283e6b2d9f5953329
Akheel <ahd.sabag@gmail.com>
2025-07-11 17:25:44 +0300

    fix sum of digits

--- katas/test/test_sum_of_digits.py
+++ katas/test/test_sum_of_digits.py
@@ -5,17 +5,17 @@ from katas.sum_of_digits import sum_of_digits
 class TestIsUnique(unittest.TestCase):
     def test_empty_str(self):
         input = ""
-        self.assertEqual(sum_of_digits([]), 0)
+        self.assertEqual(sum_of_digits(input), 0)
 
     def test_unique(self):
         input = "abc123"
-        self.assertEqual(sum_of_digits([1, 2]), 6)
+        self.assertEqual(sum_of_digits(input), 6)
 
     def test_not_unique(self):
         input = "5 cats and 2 dogs"
-        self.assertEqual(sum_of_digits([-1, 100, -39, 1092]), 7)
+        self.assertEqual(sum_of_digits(input), 7)
     
     def test_not_unique(self):
         input = "No digits here!"
-        self.assertEqual(sum_of_digits([1092]), 0)
+        self.assertEqual(sum_of_digits(input), 0)
 
